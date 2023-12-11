ba19435fd69e2e56f6fa8417c542945fe35ed428
Erin Schnabel <erinschnabel@gmail.com>
2023-12-11 16:46:20 -0500

    🎄 day 10 part 1. BETTER.

--- src/2023/day-10.py
+++ src/2023/day-10.py
@@ -1,5 +1,4 @@
-import matplotlib.path as mplPath
-import networkx as nx
+from collections import namedtuple
 import sys
 import textwrap
 import unittest
@@ -8,12 +7,35 @@ def run():
     with open("./input/2023-d10-input.txt", 'r') as file:
         input_data = [line.strip() for line in file.readlines()]
 
-    pipes = plot_pipes(input_data)
-    g, s = pipes_to_graph(pipes)
-    path = find_cycle_from_start(g, s)
-    print("** Part 1 Final: ", max_depth(path))
-
+    pipes, start = read_pipes(input_data)
+    path = walk(pipes, start)
+    print("** Part 1 Final: ", depth(path))
     print("** Part 2 Final: ", 0)
+
+def read_pipes(input_data):
+    pipes = {}
+    for r in range(len(input_data)):
+        chars = list(input_data[r])
+        for c in range(len(chars)):
+            pipes[(r, c)] = chars[c]
+            if chars[c] == 'S':
+                start = (r, c)
+    return (pipes, start)
+
+def walk(pipes, start):
+    stack = [(start, [])]
+    while stack:
+        node, visited = stack.pop()
+        visited.append(node)
+        neighbors = find_neighbors(pipes, node)
+        # print(node, pipes.get(node), neighbors, " :: ", visited)
+        for neighbor in neighbors:
+            if neighbor not in visited:
+                stack.append((neighbor, visited))
+            if len(visited) > 2 and neighbor == start:
+                print("Found a loop")
+                return visited
+    return []
 
 #  | is a vertical pipe connecting north and south.
 #  - is a horizontal pipe connecting east and west.
@@ -24,74 +46,38 @@ def run():
 #  . is ground; there is no pipe in this tile.
 #  S is the starting position of the animal
 
-def plot_pipes(input_data):
-    # plot the pipes in a 2D array
-    # return the array
-    pipes = []
-    for line in input_data:
-        pipes.append(list(line))
-    return pipes
+def hasNorth(c):
+    return c == 'S' or c == '|' or c == 'L' or c == 'J'
+def hasSouth(c):
+    return c == 'S' or c == '|' or c == '7' or c == 'F'
+def hasEast(c):
+    return c == 'S' or c == '-' or c == 'L' or c == 'F'
+def hasWest(c):
+    return c == 'S' or c == '-' or c == '7' or c == 'J'
 
-def pipes_to_graph(pipes):
-    G = nx.DiGraph()
-    start = None
-    max_i = len(pipes)
-    max_j = len(pipes[0])
-    for i in range(len(pipes)):
-        for j in range(len(pipes[i])):
-            if pipes[i][j] == '.':
-                continue
-            G.add_node((i, j), key=pipes[i][j])
-            if pipes[i][j] == 'S':
-                start = (i, j)
-            elif pipes[i][j] == '|': # vertical
-                add_edge(G, pipes, max_i, max_j, (i, j), (i-1, j), (i+1, j))
-            elif pipes[i][j] == '-': # horizontal
-                add_edge(G, pipes, max_i, max_j, (i, j), (i, j-1), (i, j+1))
-            elif pipes[i][j] == 'L': # north-to-east
-                add_edge(G, pipes, max_i, max_j, (i, j), (i-1, j), (i, j+1))
-            elif pipes[i][j] == 'J': # north-to-west
-                add_edge(G, pipes, max_i, max_j, (i, j), (i-1, j), (i, j-1))
-            elif pipes[i][j] == '7': # south-to-west
-                add_edge(G, pipes, max_i, max_j, (i, j), (i+1, j), (i, j-1))
-            elif pipes[i][j] == 'F': # south-to-east
-                add_edge(G, pipes, max_i, max_j, (i, j), (i+1, j), (i, j+1))
+def find_neighbors(pipes, node):
+    current = pipes.get(node)
+    neighbors = []
+    if current == '.':
+        return neighbors
 
-    # Treat all edges into the start as bidirectional
-    for edge in G.in_edges(start):
-        G.add_edge(edge[1], edge[0])
+    north = (node[0] - 1, node[1])
+    south = (node[0] + 1, node[1])
+    east = (node[0], node[1] + 1)
+    west = (node[0], node[1] - 1)
 
-    return G, start
+    if hasNorth(current) and hasSouth(pipes.get(north)):
+        neighbors.append(north)
+    if hasSouth(current) and hasNorth(pipes.get(south)):
+        neighbors.append(south)
+    if hasEast(current) and hasWest(pipes.get(east)):
+        neighbors.append(east)
+    if hasWest(current) and hasEast(pipes.get(west)):
+        neighbors.append(west)
 
-def add_edge(G, pipes, max_i, max_j, node, *neighbors):
-    for neighbor in neighbors:
-        i, j = neighbor
-        if 0 <= i < max_i and 0 <= j < max_j and pipes[i][j] != '.':
-            G.add_edge(node, neighbor)  # Add a directed edge
+    return neighbors
 
-def find_cycle_from_start(G, start):
-    cycles = list(nx.simple_cycles(G))
-    cycles = [cycle for cycle in cycles if start in cycle and len(cycle) > 2]
-
-    normalized_cycles = []
-    for cycle in cycles:
-        min_index = cycle.index(start)
-        normalized_cycle = cycle[min_index:] + cycle[:min_index]
-        normalized_cycles.append(normalized_cycle)
-
-    if (len(normalized_cycles) > 2):
-        print("Found multiple cycles:")
-        print(normalized_cycles)
-    elif (len(normalized_cycles) == 2):
-        if set(normalized_cycles[0]) == set(normalized_cycles[1]):
-            normalized_cycles.pop(1)
-        else:
-            print("Found multiple cycles:")
-            print(normalized_cycles)
-
-    return normalized_cycles[0]
-
-def max_depth(path):
+def depth(path):
     length = len(path)
     if length % 2 != 0:
         length += 1
@@ -107,10 +93,9 @@ class TestSolution(unittest.TestCase):
         -L-J|
         L|-JF
         """).split('\n')[1:-1]
-        pipes = plot_pipes(input_data)
-        g, s = pipes_to_graph(pipes)
-        path = find_cycle_from_start(g, s)
-        self.assertEqual(max_depth(path), 4)
+        pipes, start = read_pipes(input_data)
+        path = walk(pipes, start)
+        self.assertEqual(depth(path), 4)
 
         print("------")
         input_data = textwrap.dedent("""
@@ -120,11 +105,9 @@ class TestSolution(unittest.TestCase):
         |F--J
         LJ.LJ
         """).split('\n')[1:-1]
-        pipes = plot_pipes(input_data)
-        g, s = pipes_to_graph(pipes)
-        path = find_cycle_from_start(g, s)
-        self.assertEqual(max_depth(path), 8)
-
+        pipes, start = read_pipes(input_data)
+        path = walk(pipes, start)
+        self.assertEqual(depth(path), 8)
 
 if __name__ == "__main__":
     # Run unit tests if the script was run with the --test argument
